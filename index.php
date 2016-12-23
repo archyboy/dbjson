fd73e1831fba0095702519bb05cf7125c777c064
ArchyBoy <archy@workmail.com>
2016-12-23 10:51:54 +0100

    More code cleaning

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\Finder\Finder as Finder;
+
 set_include_path('classes/');
 spl_autoload($class);
 spl_autoload_extensions('.php');
@@ -12,7 +14,7 @@ require 'vendor/autoload.php';
 echo file_get_contents('test.html');
 
 $dbjson = new DBjson($config);
-$finder = Finder;
+$finder = new Finder;
 
 //$dbjson->testFinder();
 //$dbjson->testFilesystem();
@@ -31,14 +33,13 @@ $collection_array = $dbjson->getCollection();
 
 $filename = $collection_array[0];
 
-echo 'Filename:'.$filename;
+echo 'Filename:' . $filename;
 
 //insert_test_data($dbjson);
 //$lorum_content_json = json_encode($content = file_get_contents('http://loripsum.net/api/1'));
 //$dbjson->insertDocument($lorum_content_json);
 
-function insert_test_data($dbjson)
-{
+function insert_test_data($dbjson) {
     $database_size_bytes = Helper::dir_size('dbdata');
     while ($database_size_bytes < 100000) {
         //for($i=0; $i < 1000; $i) {
@@ -61,7 +62,7 @@ $database_size_megabytes = $database_size_kilobytes / 1000;
 $database_size_gigabytes = $database_size_megabytes / 1000;
 $database_size_terrabytes = $database_size_gigabytes / 1000;
 
-echo '<br>Database size: '.number_format($database_size_bytes, 2).' bytes<br>';
+echo '<br>Database size: ' . number_format($database_size_bytes, 2) . ' bytes<br>';
 
 //$document_array = json_decode($document_json, true);
 //Helper::print_pre($document_array);
@@ -72,12 +73,12 @@ echo $document_json_lorum;
 
 $document_object = json_decode($document_json_contact);
 Helper::print_pre($document_object);
-echo '<br>Firstname: '.$document_object->firstname;
-echo '<br>Lastname: '.$document_object->lastname;
-echo '<br>Street: '.$document_object->adress->street;
-echo '<br>Postcode: '.$document_object->adress->postcode;
-echo '<br>State: '.$document_object->adress->state;
-echo '<br>Country: '.$document_object->adress->country;
+echo '<br>Firstname: ' . $document_object->firstname;
+echo '<br>Lastname: ' . $document_object->lastname;
+echo '<br>Street: ' . $document_object->adress->street;
+echo '<br>Postcode: ' . $document_object->adress->postcode;
+echo '<br>State: ' . $document_object->adress->state;
+echo '<br>Country: ' . $document_object->adress->country;
 
 //$collection_array = $dbjson->getCollection();
 //Helper::print_pre($collection_array);
