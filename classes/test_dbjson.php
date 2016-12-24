a066198b53e03a900056d9af0bede30b3dbc1062
ArchyBoy <archy@workmail.com>
2016-12-24 04:01:40 +0100

    Adding a new class for running data dummy testing

--- classes/test_dbjson.php
+++ classes/test_dbjson.php
@@ -1,12 +1,23 @@
 <?php
 
-require_once 'class_dbjson.php';
-
 class Test_DBjson extends DBjson {
 
-  public function __construct($config) {
+    public function __construct() {
+        parent::__construct();
+    }
 
-  parent::__construct($config);
-  }
+    public function insert_test_data($dbjson, $data, $bytes) {
+        //Helper::print_pre($dummy_json_objects_array);
+        do {
+            $database_insert_size_bytes += $dbjson->insertDocument($data);
+            $database_insert_documents++;
+            //echo '<br>Database size: ' . number_format($database_insert_size_bytes, 2) . ' bytes<br>';
+        } while ($database_insert_size_bytes < $bytes);
+
+        return array(
+            'size' => $database_insert_size_bytes,
+            'documents' => $database_insert_documents
+        );
+    }
 
 }
