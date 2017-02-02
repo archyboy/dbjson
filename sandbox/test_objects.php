4c8877116ebb8d9c3d6feb5a17b8f845b711224d
ArchyBoy <archy@workmail.com>
2017-02-02 06:12:20 +0100

    Removed more files not in use from repository

--- sandbox/test_objects.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$obj = new stdObject();
-$obj->name = "Nick";
-
-$obj->getInfo = function($stdObject) { // $stdObject referred to this object (stdObject).
-    echo $stdObject->name . " " . $stdObject->surname . " have " . $stdObject->age . " yrs old. And live in " . $stdObject->adresse;
-};
